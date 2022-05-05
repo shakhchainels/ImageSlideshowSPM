8f63162ca51fcd7342f3ee3bae622c1b1eb9a064
Andre Nguyen <andre@chainels.com>
2022-05-05 16:32:21 +0200

    adding code

--- Package.swift
+++ Package.swift
@@ -5,24 +5,52 @@ import PackageDescription
 
 let package = Package(
     name: "ImageSlideshowSPM",
+    platforms: [
+        .iOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ImageSlideshowSPM",
-            targets: ["ImageSlideshowSPM"]),
+            name: "ImageSlideshow",
+            targets: ["ImageSlideshow"]),
+        .library(
+            name: "ImageSlideshow/Kingfisher",
+            targets: ["ImageSlideshowKingfisher"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.2.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ImageSlideshowSPM",
-            dependencies: []),
+            name: "ImageSlideshow",
+            dependencies: ["Kingfisher"],
+            sources: [
+                "Classes/Core/ActivityIndicator.swift",
+                "Classes/Core/Bundle+Module.swift",
+                "Classes/Core/FullScreenSlideshowViewController.swift",
+                "Classes/Core/ImageSlideshow.swift",
+                "Classes/Core/ImageSlideshowItem.swift",
+                "Classes/Core/InputSource.swift",
+                "Classes/Core/PageIndicator.swift",
+                "Classes/Core/PageIndicatorPosition.swift",
+                "Classes/Core/SwiftSupport.swift",
+                "Classes/Core/UIImage+AspectFit.swift",
+                "Classes/Core/UIImageView+Tools.swift",
+                "Classes/Core/ZoomAnimatedTransitioning.swift",
+            ],
+            resources: [
+                .copy("Assets/ic_cross_white@2x.png"),
+                .copy("Assets/ic_cross_white@3x.png"),
+            ]),
+        .target(
+            name: "ImageSlideshowKingfisher",
+            dependencies: ["ImageSlideshow", "Kingfisher"],
+            path: "Sources/ImageSlideshow/Classes/InputSources",
+            sources: ["KingfisherSource.swift"]),
         .testTarget(
             name: "ImageSlideshowSPMTests",
-            dependencies: ["ImageSlideshowSPM"]),
+            dependencies: ["ImageSlideshow"])
     ]
 )
--- Tests/ImageSlideshowSPMTests/ImageSlideshowSPMTests.swift
+++ Tests/ImageSlideshowSPMTests/ImageSlideshowSPMTests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import ImageSlideshowSPM
+@testable import ImageSlideshow
 
 final class ImageSlideshowSPMTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ImageSlideshowSPM().text, "Hello, World!")
+        XCTAssertEqual(ImageSlideshow().currentPage, 0)
     }
 }
